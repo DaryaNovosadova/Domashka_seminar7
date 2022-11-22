029248adb3ba5feefc6268b35402df460de0124b
Darya <darya.novosadova@gmail.com>
2022-11-22 17:45:16 +0700

    Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,43 @@
+﻿int rows = 4;
+int columns = 4;
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j], 4} ");
+        }
+        Console.WriteLine();
+    }
+}
+double average = 0;
+for(int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = sum + array[i, j];
+    }
+    average = sum / array.GetLength(0);
+    int n = j + 1;
+    Console.WriteLine($"Среднее арифметическое {n} столбца {average}");
+}
+     
